0ae8f10ce85d59095cf809b4d08b02a99038f688
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2021-03-16 14:19:06 -0400

    Removed files that are no longer needed

--- example2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-import hubmapbags
-
-data_provider = 'University of Florida'
-metadata_file = 'example.csv'
-
-hubmapbags.magic.do_it( data_provider, metadata_file )
