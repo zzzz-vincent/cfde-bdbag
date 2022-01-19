21bc2b066eb7714815f767a2c0edbdc9de07be31
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2022-01-19 16:09:51 -0500

    Updated test

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [ -d codex-published-0dc91e6484430c2db25b6765dd6aa565 ]; then
+	rm -r codex-published-0dc91e6484430c2db25b6765dd6aa565
+fi
+
 python3 codex.py
+
+if [ -d codex-published-0dc91e6484430c2db25b6765dd6aa565 ]; then
+	exit 0
+else
+	echo "Folder "codex-published-0dc91e6484430c2db25b6765dd6aa565" not found."
+	exit 1
+fi
