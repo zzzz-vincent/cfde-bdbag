0c47dafd59198082791c7b5f76493b9a58773b08
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2022-04-06 13:41:47 -0400

    Added useful scripts

---
+++ remove_done_files.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+import sys
+
+def main( metadata_file ):
+    datasets = pd.read_csv( metadata_file )
+
+    for dataset in datasets.iterrows():
+        dataset = dataset[1]
+        data_directory = dataset['full_path']
+        computing = data_directory.replace('/','_').replace(' ','_') + '.computing'
+        done = data_directory.replace('/','_').replace(' ','_') + '.done'
+        if Path( done ).is_file():
+            print( 'Removing file ' + done )
+            Path( done ).unlink()
+
+if __name__ == "__main__":
+    metadata_file = sys.argv[1]
+    main( metadata_file )
