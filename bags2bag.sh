7d736e9d314ca6d7d254e3970e855f88dab03171
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-04-18 13:50:48 -0400

    Updated bags2bag

--- bags2bag.sh
+++ bags2bag.sh
@@ -4,7 +4,7 @@ set -x
 
 DATA=$1
 
-OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
 if [ -d $OUTPUT_DIRECTORY ]; then
 	rm -rf $OUTPUT_DIRECTORY
 fi
@@ -12,7 +12,7 @@ mkdir $OUTPUT_DIRECTORY
 
 function create_subject_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/subject.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/subject.tsv
         fi
@@ -22,7 +22,7 @@ function create_subject_tsv(){
 
 function create_subject_in_collection_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/subject_in_collection.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/subject_in_collection.tsv
         fi
@@ -32,7 +32,7 @@ function create_subject_in_collection_tsv(){
 
 function create_file_in_collection_tsv(){
        	FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
        	if [ ! -f $OUTPUT_DIRECTORY/file_in_collection.tsv ]; then
                	head -n1 $FILE > $OUTPUT_DIRECTORY/file_in_collection.tsv
         fi
@@ -42,7 +42,7 @@ function create_file_in_collection_tsv(){
 
 function create_file_tsv(){
 	FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
 	if [ ! -f $OUTPUT_DIRECTORY/file.tsv ]; then
 		head -n1 $FILE > $OUTPUT_DIRECTORY/file.tsv
 	fi
@@ -50,9 +50,9 @@ function create_file_tsv(){
 	tail -n +2 $FILE >> $OUTPUT_DIRECTORY/file.tsv
 }
 
-function create_files_in_collection_tsv(){
+function create_file_in_collection_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/files_in_collection.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/files_in_collection.tsv
         fi
@@ -62,7 +62,7 @@ function create_files_in_collection_tsv(){
 
 function create_biosample_tsv(){
 	FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
 	if [ ! -f $OUTPUT_DIRECTORY/biosample.tsv ]; then
 		head -n1 $FILE > $OUTPUT_DIRECTORY/biosample.tsv
 	fi
@@ -72,7 +72,7 @@ function create_biosample_tsv(){
 
 function create_file_describes_biosample_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/file_describes_biosample.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/file_describes_biosample.tsv
         fi
@@ -82,7 +82,7 @@ function create_file_describes_biosample_tsv(){
 
 function create_biosample_in_collection_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/biosample_in_collection.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/biosample_in_collection.tsv
         fi
@@ -92,7 +92,7 @@ function create_biosample_in_collection_tsv(){
 
 function create_biosample_from_subject_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/biosample_from_subject.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/biosample_from_subject.tsv
         fi
@@ -102,7 +102,7 @@ function create_biosample_from_subject_tsv(){
 
 function create_collection_defined_by_project_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/collection_defined_by_project.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/collection_defined_by_project.tsv
         fi
@@ -112,7 +112,7 @@ function create_collection_defined_by_project_tsv(){
 
 function create_file_describes_subject_tsv(){
         FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
         if [ ! -f $OUTPUT_DIRECTORY/file_describes_subject.tsv ]; then
                 head -n1 $FILE > $OUTPUT_DIRECTORY/file_describes_subject.tsv
         fi
@@ -122,23 +122,36 @@ function create_file_describes_subject_tsv(){
 
 function create_collection_tsv(){
 	FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
-	if [ ! -f $OUTPUT_DIRECTORY/temp.tsv ]; then
-		head -n1 $FILE > $OUTPUT_DIRECTORY/temp.tsv
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
+	if [ ! -f $OUTPUT_DIRECTORY/collection.tsv ]; then
+		head -n1 $FILE > $OUTPUT_DIRECTORY/collection.tsv
 	fi
 
-	tail -n +2 $FILE >> $OUTPUT_DIRECTORY/temp.tsv
+	tail -n +2 $FILE >> $OUTPUT_DIRECTORY/collection.tsv
 }
 
 function create_project_tsv(){
 	FILE=$1
-        OUTPUT_DIRECTORY='cfde-submission'-$(date +"%Y%m%d")
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
 	
-	if [ ! -f $OUTPUT_DIRECTORY/temp.tsv ]; then
-	        cp project.tsv $OUTPUT_DIRECTORY/temp.tsv
+	if [ ! -f $OUTPUT_DIRECTORY/project.tsv ]; then
+	        #cp project.tsv $OUTPUT_DIRECTORY/project.tsv
+		head -n1 $FILE > $OUTPUT_DIRECTORY/project.tsv
 	fi
 
-	tail -n +2 $FILE >> $OUTPUT_DIRECTORY/temp.tsv
+	tail -n +2 $FILE >> $OUTPUT_DIRECTORY/project.tsv
+}
+
+function create_project_in_project_tsv(){
+	FILE=$1
+        OUTPUT_DIRECTORY='cfde-bdbag'-$(date +"%Y%m%d")
+	
+	if [ ! -f $OUTPUT_DIRECTORY/project_in_project.tsv ]; then
+	        #cp project_in_project.tsv $OUTPUT_DIRECTORY/project_in_project.tsv
+		head -n1 $FILE > $OUTPUT_DIRECTORY/project_in_project.tsv
+	fi
+
+	tail -n +2 $FILE >> $OUTPUT_DIRECTORY/project_in_project.tsv
 }
 
 export -f create_file_tsv
@@ -154,60 +167,37 @@ export -f create_file_describes_biosample_tsv
 export -f create_file_describes_subject_tsv
 export -f create_biosample_from_subject_tsv
 export -f create_collection_defined_by_project_tsv
+export -f create_project_in_project_tsv
 
 find $DATA -type f -name 'file.tsv' -exec bash -c 'create_file_tsv "$0"' {} \;
-
-find $DATA -type f -name 'subject_in_collection.tsv' -exec bash -c 'create_subject_in_collection_tsv "$0"' {} \;
-
-find $DATA -type f -name 'biosample_in_collection.tsv' -exec bash -c 'create_biosample_in_collection_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/biosample_in_collection.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/biosample_in_collection.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
-
 find $DATA -type f -name 'file_describes_biosample.tsv' -exec bash -c 'create_file_describes_biosample_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/file_describes_biosample.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/file_describes_biosample.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
-
 find $DATA -type f -name 'file_in_collection.tsv' -exec bash -c 'create_file_in_collection_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/file_in_collection.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/file_in_collection.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
-
-find $DATA -type f -name 'files_in_collection.tsv' -exec bash -c 'create_files_in_collection_tsv "$0"' {} \;
-
-find $DATA -type f -name 'project.tsv' -exec bash -c 'create_project_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/temp.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/project.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
-
-find $DATA -type f -name 'collection.tsv' -exec bash -c 'create_collection_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/temp.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/collection.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
+find $DATA -type f -name 'file_describes_subject.tsv' -exec bash -c 'create_file_describes_subject_tsv "$0"' {} \;
 
 find $DATA -type f -name 'subject.tsv' -exec bash -c 'create_subject_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/subject.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/subject.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
+cat $OUTPUT_DIRECTORY/subject.tsv | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/subject.tsv
+find $DATA -type f -name 'subject_in_collection.tsv' -exec bash -c 'create_subject_in_collection_tsv "$0"' {} \;
+cat $OUTPUT_DIRECTORY/subject_in_collection.tsv | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/subject_in_collection.tsv
 
+find $DATA -type f -name 'biosample_in_collection.tsv' -exec bash -c 'create_biosample_in_collection_tsv "$0"' {} \;
+cat $OUTPUT_DIRECTORY/biosample_in_collection.tsv | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/biosample_in_collection.tsv
 find $DATA -type f -name 'biosample.tsv' -exec bash -c 'create_biosample_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/biosample.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/biosample.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
-
+cat $OUTPUT_DIRECTORY/biosample.tsv | sort | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/biosample.tsv
 find $DATA -type f -name 'biosample_from_subject.tsv' -exec bash -c 'create_biosample_from_subject_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/biosample_from_subject.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/biosample_from_subject.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
+cat $OUTPUT_DIRECTORY/biosample_from_subject.tsv | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/biosample_from_subject.tsv
 
-find $DATA -type f -name 'file_describes_subject.tsv' -exec bash -c 'create_file_describes_subject_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/file_describes_subject.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/file_describes_subject.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
+find $DATA -type f -name 'project.tsv' -exec bash -c 'create_project_tsv "$0"' {} \;
+cat $OUTPUT_DIRECTORY/project.tsv | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/project.tsv
+find $DATA -type f -name 'project_in_project.tsv' -exec bash -c 'create_project_in_project_tsv "$0"' {} \;
+cat $OUTPUT_DIRECTORY/project_in_project.tsv | sort | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/project_in_project.tsv
 
+find $DATA -type f -name 'collection.tsv' -exec bash -c 'create_collection_tsv "$0"' {} \;
+cat $OUTPUT_DIRECTORY/collection.tsv | uniq > /tmp/temp.tsv && mv /tmp/temp.tsv $OUTPUT_DIRECTORY/collection.tsv
 find $DATA -type f -name 'collection_defined_by_project.tsv' -exec bash -c 'create_collection_defined_by_project_tsv "$0"' {} \;
-cat $OUTPUT_DIRECTORY/collection_defined_by_project.tsv | perl -ne 'print unless $seen{$_}++' > $OUTPUT_DIRECTORY/temp.tsv
-mv $OUTPUT_DIRECTORY/temp.tsv $OUTPUT_DIRECTORY/collection_defined_by_project.tsv
-rm -f $OUTPUT_DIRECTORY/temp.tsv
 
-cp project_in_project.tsv $OUTPUT_DIRECTORY/
-cp primary_dcc_contact.tsv $OUTPUT_DIRECTORY/
+find $DATA -type f -name "dcc.tsv" -exec cp {} $OUTPUT_DIRECTORY/ \;
+
+bash ./copy_empty_files.sh $OUTPUT_DIRECTORY
+
+find $DATA -type f -name "*.tsv" -exec sed -i 's/hubmapconsortium.org,2021/hubmapconsortium.org,2022/g' {} \;
+
